41693aa3a0838bc1625109cbe606aba85bcef0cd
sindhukeshary14 <sindhukeshary14@gmail.com>
2023-10-29 19:11:33 +0000

    newchangesss

--- my-react-app/src/components/QuizIndex.js
+++ my-react-app/src/components/QuizIndex.js
@@ -4,13 +4,8 @@ function QuizIndex() {
   return (
     <div className='body'>
       <h2>Welcome to the Quiz App</h2>
-      <div className='body'>
-        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
-          <ol className="carousel-indicators">
-            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active"></li>
-            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
-          </ol>
+       <div className='body'> 
+        <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
           <div className="carousel-inner">
             <div className="carousel-item active">
               <img src="https://a.storyblok.com/f/47007/2500x1309/0fd8d4feb6/221103_app_onboarding_v03_meta.png" className="d-block w-100" alt="Image 1" />
@@ -22,12 +17,12 @@ function QuizIndex() {
               <img src="https://cdn.dribbble.com/users/1752777/screenshots/12018136/prestation_quiz_app_d.png" className="d-block w-100" alt="Image 3" />
             </div>
           </div>
-          <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+          <a className="carousel-control-prev" href="#carouselExample" role="button" data-bs-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Previous</span>
           </a>
-          <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <a className="carousel-control-next" href="#carouselExample" role="button" data-bs-slide="next">
+            <span className="carousel-control-next-icon" ariahidden="true"></span>
             <span className="visually-hidden">Next</span>
           </a>
         </div>
@@ -38,11 +33,11 @@ function QuizIndex() {
             <h2>A simple quiz platform for all types of learning and checking your brain.</h2>
           </div>
           <div className='col col-md-6'>
-            <img src="https://media.tenor.com/CeDk6XdCgOUAAAAj/develop-web.gif" alt="Developing Web" />
+            <img src="https://media.tenor.com/CeDk6XdCgOUAAAAj/develop-web.gif" alt="Developing Web"></img>
           </div>
         </div>
       </div>
-    </div>
+    // </div>
   );
 }
 
